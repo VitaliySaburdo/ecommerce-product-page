b00f3fd434faa1c51075ab189fd99c3b0e4a82c7
VitaliySaburdo <vsaburdo@gmail.com>
2024-08-21 21:30:35 +0300

    add

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,11 @@
-import productImage from '../assets/images/image-product-1-thumbnail.jpg';
-import { Container } from './Container';
+// import { Container } from './Container';
 import { Header } from './Header/Header';
 function App() {
   return (
     <>
       <Header />
       <main>
-        <section>
-          <Container>
-            <h1>Hello</h1>
-            <img src={productImage} alt="" />
-          </Container>
-        </section>
+        <section></section>
       </main>
     </>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,38 +2,44 @@ import { Container } from '../Container';
 import logo from '../../assets/images/logo.svg';
 import cart from '../../assets/images/icon-cart.svg';
 import avatar from '../../assets/images/image-avatar.png';
-import style from './Header.scss';
+import style from './Header.module.scss';
 
 export const Header = () => {
   return (
     <>
       <Container>
-        <header>
-          <img src={logo} alt="logo" />
+        <header className={style.header}>
+          <img
+            className={style.logo}
+            src={logo}
+            alt="logo"
+            width={140}
+            height={20}
+          />
           <nav>
-            <ul className="list">
+            <ul className={style.list}>
               <li className="item">
-                <a href="#" className="link">
+                <a href="#" className={style.link}>
                   Collections
                 </a>
               </li>
               <li className="item">
-                <a href="#" className="link">
+                <a href="#" className={style.link}>
                   Men
                 </a>
               </li>
               <li className="item">
-                <a href="#" className="link">
+                <a href="#" className={style.link}>
                   Women
                 </a>
               </li>
               <li className="item">
-                <a href="#" className="link">
+                <a href="#" className={style.link}>
                   About
                 </a>
               </li>
               <li className="item">
-                <a href="#" className="link">
+                <a href="#" className={style.link}>
                   Contact
                 </a>
               </li>
