d339fd21e6063530c473f643d72127594f7ee383
Vitaliy Saburdo <vsaburdo@gmail.com>
2024-08-21 16:36:33 +0300

    add

---
+++ src/assets/components/App.tsx
@@ -0,0 +1,11 @@
+import productImage from '../images/image-product-4-thumbnail.jpg';
+function App() {
+  return (
+    <>
+      <h1>Hello</h1>
+      <img src={productImage} alt="" />
+    </>
+  );
+}
+
+export default App;
